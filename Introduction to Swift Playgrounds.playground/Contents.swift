373daaf3b8a267b90ba8b145571bac30f7f22aa7
marionatuhwo <ano@hawaii.edu>
2023-08-29 13:35:35 -1000

    Completed Part 1 to Part 8
    Still need to complete On your own!

--- Introduction to Swift Playgrounds.playground/Contents.swift
+++ Introduction to Swift Playgrounds.playground/Contents.swift
@@ -2,6 +2,29 @@ import UIKit
 
 var greeting = "Hello, playground"
 
+//var myFirstVariable = "Iʻve created my first variable"
+//var is only used when declaring the variable
+var myFirstVariable: String
+
+myFirstVariable = "Iʻve created my first variable"
+//myFirstVariable = "5"
+
+/* use this to comment a block of code*/
+//this is how to comment out a single line of code
+
+var mySecondVariable = " and look I added 2 strings together"
+
+//Part 7 String Concantenation
+//essentially myFirstVariable + mySecondVariable
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+
+print(myThirdVariable)
+
+
+
+
+
 
 
 
