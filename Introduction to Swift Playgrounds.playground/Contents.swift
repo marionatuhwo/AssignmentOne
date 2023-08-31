83cf018c79c8df6a8ff4b989bf52081f85d9fc76
marionatuhwo <ano@hawaii.edu>
2023-08-31 13:09:18 -1000

    Assignment #3 Completed parts 1-6
    Learned about Boolean Variables

--- Introduction to Swift Playgrounds.playground/Contents.swift
+++ Introduction to Swift Playgrounds.playground/Contents.swift
@@ -21,6 +21,27 @@ var myThirdVariable = myFirstVariable + mySecondVariable
 
 print(myThirdVariable)
 
+//Assignment #3 Introduction to True and False
+
+var appState = false
+print(appState)
+
+//Part 4
+//create 2 new variables of type int
+
+var numberOne = 5
+var numberTwo = 7
+
+//We are going to create a statement that compares the two variables and assign that statement to a third variable
+
+var comparison = numberOne > numberTwo
+
+//Part 7 On Your Own Part 1:
+
+
+
+
+//Part 8 On Your Own Part 2
 
 
 
